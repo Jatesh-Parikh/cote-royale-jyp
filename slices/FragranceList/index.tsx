6367d8226a384304492474be57e2c810c62bf56b
Jatesh Parikh <jateshparikh97@gmail.com>
2025-12-09 21:38:19 +0530

    FragranceList slice added

--- slices/FragranceList/index.tsx
+++ slices/FragranceList/index.tsx
@@ -1,6 +1,10 @@
 import { FC } from "react";
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { Content, isFilled } from "@prismicio/client";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+
+import { Bounded } from "@/components/Bounded";
+import { RevealText } from "@/components/RevealText";
+import { FragranceDisplay } from "./FragranceDisplay";
 
 /**
  * Props for `FragranceList`.
@@ -13,19 +17,44 @@ export type FragranceListProps =
  */
 const FragranceList: FC<FragranceListProps> = ({ slice }) => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="space-y-8 bg-black py-16 text-center text-white md:py-24"
     >
-      Placeholder component for fragrance_list (variation: {slice.variation})
-      slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use the Prismic MCP server with your code editor
-       * 📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
-    </section>
+      <div className="mx-auto space-y-8">
+        <p className="text-sm font-light tracking-[0.2em] uppercase">
+          {slice.primary.eyebrow}
+        </p>
+
+        <RevealText
+          field={slice.primary.heading}
+          as="h2"
+          id={`fragrance-list-heading-${slice.id}`}
+          align="center"
+          duration={1.5}
+          staggerAmount={0.3}
+          className="font-display text-5xl uppercase sm:text-6xl md:text-7xl lg:text-8xl"
+        />
+
+        <div className="mx-auto max-w-2xl text-lg text-balance text-gray-300">
+          <PrismicRichText field={slice.primary.body} />
+        </div>
+
+        <div className="mt-12 grid grid-cols-1 gap-12">
+          {slice.primary.fragrances.map((item) => {
+            if (isFilled.contentRelationship(item.fragrance)) {
+              return (
+                <FragranceDisplay
+                  key={item.fragrance.id}
+                  id={item.fragrance.id}
+                />
+              );
+            }
+          })}
+        </div>
+      </div>
+    </Bounded>
   );
 };
 
