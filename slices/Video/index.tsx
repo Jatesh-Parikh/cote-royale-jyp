ec6dab55435297d41a15557c0cb47c6c48166f5a
Jatesh Parikh <jateshparikh97@gmail.com>
2025-12-14 12:36:10 +0530

    Home page completed

--- slices/Video/index.tsx
+++ slices/Video/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import { Lazy } from "@/components/Lazy";
 
 /**
  * Props for `Video`.
@@ -15,14 +16,20 @@ const Video: FC<VideoProps> = ({ slice }) => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="bg-black"
     >
-      Placeholder component for video (variation: {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use the Prismic MCP server with your code editor
-       * 📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
+      <h2 className="sr-only">Cote Royale Video Reel</h2>
+
+      <Lazy
+        rootMargin="1500px"
+        className="relative h-screen overflow-hidden md:aspect-video md:h-auto"
+      >
+        <iframe
+          src={`https://www.youtube-nocookie.com/embed/${slice.primary.youtube_video_id}?autoplay=1&mute=1&loop=1&playlist=${slice.primary.youtube_video_id}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          className="pointer-events-none absolute left-1/2 aspect-video h-full -translate-x-1/2"
+        />
+      </Lazy>
     </section>
   );
 };
